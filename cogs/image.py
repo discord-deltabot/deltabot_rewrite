02ab550b987203eb5692d5fe3d1a275c6bdb234a
Bhagyajkumar Bijukumar <bhagyajbijukumar@gmail.com>
2021-05-06 20:51:34 +0530

    forgon to await r.content

--- cogs/image.py
+++ cogs/image.py
@@ -13,19 +13,18 @@ class Image(commands.Cog):
         if image is None:
             image_url = ctx.author.avatar_url
             with self.bot.session.get(image_url) as r:
-                img_bytes = r.content()
+                img_bytes = await r.content()
         try:
-            member = await commands.MemberConverter().convert(ctx=ctx,argument=image)
+            member = await commands.MemberConverter().convert(ctx=ctx, argument=image)
             image_url = member.avatar_url
             with self.bot.session.get(image_url) as r:
-                img_bytes = r.content()
+                img_bytes = await r.content()
         except:
             return await ctx.send("Emoji inverting coming soon")
 
         partial_function = functools.partial(image_helper.get_inverted_bytes, img_bytes)
-        inverted_bytes = await self.bot.loop.run_in_executor(None,partial_function)
+        inverted_bytes = await self.bot.loop.run_in_executor(None, partial_function)
         await ctx.send(file=discord.File(fp=inverted_bytes, filename="inverted.png"))
-
 
 
 def setup(bot):
