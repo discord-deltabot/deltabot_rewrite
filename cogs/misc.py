6dd16196db256e77724fdae23b85caeeb8c87ca0
Bhagyajkumar Bijukumar <bhagyajbijukumar@gmail.com>
2021-05-17 19:29:04 +0530

    added meme command

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import time
+import aiohttp
+import random
 
 class Misc(commands.Cog):
     def __init__(self, bot):
@@ -10,6 +12,18 @@ class Misc(commands.Cog):
     async def remind(self,ctx, seconds, *, reason:str=None):
         await ctx.reply(f"Ok! I will remind you for {reason} in {seconds} second(s)",mention_author=False)
         await self.bot.create_user_schedule(time=time.time()+float(seconds), message=f"Hey you just asked me to remind you for `{reason}` now", user=ctx.author)
+
+    @commands.command()
+    async def meme(self, ctx):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get("https://www.reddit.com/r/memes.json") as r:
+                result = await r.json()
+        index = random.randint(0,len(result["data"]["children"]))
+        image_url = result["data"]["children"][index]["data"]["url"]
+        title = result["data"]["children"][index]["data"]["title"]
+        embed = discord.Embed(title=title, color=self.bot.default_color)
+        embed.set_image(url=image_url)
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
--- utils/image_helper.py
+++ utils/image_helper.py
@@ -1,9 +1,13 @@
 import PIL.Image, PIL.ImageOps
 from io import BytesIO
+import requests
 
+def get_image_from_url(url):
+    coin = requests.get(url).content
+    source = BytesIO(coin)
+    return PIL.Image.open(source)
 
 def get_inverted_bytes(imagebytes):
-    print("function reached")
     with PIL.Image.open(imagebytes) as img:
         out = PIL.ImageOps.invert(img.convert("RGB"))
     output_buffer = BytesIO()
