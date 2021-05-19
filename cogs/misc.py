f374f9de1ea3365c1d4df5c5342e5e95934423e5
Bhagyajkumar Bijukumar <bhagyajbijukumar@gmail.com>
2021-05-19 14:05:21 +0530

    fix

--- cogs/misc.py
+++ cogs/misc.py
@@ -15,12 +15,12 @@ class Misc(commands.Cog):
         await self.bot.db.fetchval("SELECT 1")
         database_latency = (time.time() - dbt1) * 1000
         embed = discord.Embed(title="PIng", color=self.bot.default_color)
-        embed.add_field(name="websocket Latency", value=f"```{str(round(websocket_latency))} ms```", inline=False)
-        embed.add_field(name="database latency", value=f"```{str(round(database_latency))} ms```", inline=False)
+        embed.add_field(name="websocket Latency", value=f"```js\n{str(round(websocket_latency))} ms```", inline=False)
+        embed.add_field(name="database latency", value=f"```js\n{str(round(database_latency))} ms```", inline=False)
         mest1 = time.time()
         mes = await ctx.send(embed=embed)
         message_latency = (time.time() - mest1) * 1000
-        embed.add_field(name="Typing", value=f"```{str(round(message_latency))} ms```")
+        embed.add_field(name="Typing", value=f"```js\n{str(round(message_latency))} ms```")
         await mes.edit(embed=embed)
 
 
